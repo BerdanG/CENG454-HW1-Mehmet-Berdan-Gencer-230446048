522e305e3a52c62fc976f3510ca392271875c7b6
BerdanG <berdangencer@yahoo.com>
2026-03-20 06:06:53 +0300

    feat: setup FlightController and cache Rigidbody

---
+++ Assets/Scripts/FlightController.cs
@@ -0,0 +1,46 @@
+// FlightController.cs
+// CENG 454 – HW1: Sky-High Prototype
+// Author: Mehmet Berdan Gençer | Student ID: 230446048
+
+
+using UnityEditor.Callbacks;
+using UnityEngine;
+public class FlightController : MonoBehaviour
+{
+    [SerializeField] private float pitchSpeed = 45f; // degrees/second
+    [SerializeField] private float yawSpeed = 45f; // degrees/second
+    [SerializeField] private float rollSpeed = 45f; // degrees/second
+    [SerializeField] private float thrustSpeed = 5f; // units/second
+    
+    private Rigidbody rb;
+    
+    
+    void Start()
+    {
+    // Why is freezeRotation needed? Answer in your PDF.
+    rb = GetComponent<Rigidbody>();
+    rb.freezeRotation = true;
+    }
+ 
+
+    void Update()// or FixedUpdate()
+    {
+    HandleRotation();
+    HandleThrust();
+    }
+
+
+     private void HandleRotation()
+    {
+    // TODO (Task 3-C):
+    // Pitch
+    // Roll
+    }
+ 
+ 
+    private void HandleThrust()
+    {
+    // TODO (Task 3-D):
+    }
+}
+
